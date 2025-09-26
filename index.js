1b795da21dab42bf267db80a7d456d63c85165c5
AlejoDiazzz <diaza.alejandro123@gmail.com>
2025-09-25 19:13:05 -0500

    Interfaz Simple

--- index.js
+++ index.js
@@ -5,6 +5,8 @@ import peliculaRoutes from './routes/pelicula.mjs';
 import authRoutes from './routes/auth.mjs';
 import usuarioRoutes from './routes/usuario.mjs'; // Importar rutas de usuario
 import dotenv from 'dotenv';
+import path from 'path'; // Importar path
+import { fileURLToPath } from 'url'; // Importar fileURLToPath
 
 // Swagger
 import swaggerUi from 'swagger-ui-express';
@@ -20,6 +22,9 @@ app.set('PORT', process.env.PORT || 6972);
 // Middleware
 app.use(express.json());
 
+// Servir archivos estáticos desde la carpeta 'public'
+app.use(express.static('public'));
+
 // Connect database
 connectDB();
 
@@ -33,7 +38,17 @@ app.use('/api/peliculas', peliculaRoutes);
 app.use('/api/auth', authRoutes);
 app.use('/api/usuarios', usuarioRoutes); // Usar rutas de usuario
 
+// Definir __dirname para módulos ES
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+// Ruta para servir la interfaz de prueba
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'views', 'index.html'));
+});
+
 // servidor
 app.listen(app.get('PORT'), () => {
   console.log(`Server listening on port ${app.get('PORT')}`);
 });
+
